8b8f5c584b42ed93bfdd2610ed7bfb725b6c0bdd
Vanavski <ekvator-r@mail.ru>
2019-11-13 13:36:43 +0300

    Add PingPong for 2 ANN which plays 1 by 1. Also can switch on to player mod. Changes: new brain, UI with score, paddle controller for player, serve ball script for sum score

---
+++ ML GENS/Assets/Pong NN/PaddleControls.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleControls : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("up"))
+			this.transform.Translate(0,0.3f,0);
+		else if(Input.GetKey("down"))
+			this.transform.Translate(0,-0.3f,0);
+	}
+}
---
+++ ML GENS/Assets/Pong NN/ServeBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServeBall : MonoBehaviour {
+
+	public GameObject ball;
+
+	public bool backWall = false;
+	public BrainPong2 b;
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "ball" && backWall)
+		{
+			b.numMissed += 1;
+			ball.GetComponent<MoveBall>().ResetBall();
+		}
+	}
+}
